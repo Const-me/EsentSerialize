c5b33f63d9af31d4759668889ebce6cac1be4fdd
Konstantin <soonts@live.com>
2016-05-20 21:59:51 +0200

    Translated demo to LINQ-like queries

--- Demos/ConsoleDemo/Program.cs
+++ Demos/ConsoleDemo/Program.cs
@@ -10,13 +10,13 @@ namespace ConsoleDemo
 		static void PopulateDemoDatabase( iSerializerSession sess )
 		{
 			Person[] personsTest = new Person[]
-		{
-			new Person( Person.eSex.Female, "Jenifer Smith", new string[0] ),
-			new Person( Person.eSex.Male, "Konstantin", new string[]{ "+7 926 139 63 18" } ),
-			new Person( Person.eSex.Male, "John Smith", new string[]{ "+1 800 123 4567", "+1 800 123 4568" } ),
-			new Person( Person.eSex.Female, "Mary Jane", new string[]{ "555-1212" } ),
-			new Person( Person.eSex.Other, "Microsoft", new string[]{ "+1 800 642 7676", "1-800-892-5234" } ),
-		};
+			{
+				new Person( Person.eSex.Female, "Jenifer Smith", new string[0] ),
+				new Person( Person.eSex.Male, "Konstantin", new string[]{ "+7 926 139 63 18" } ),
+				new Person( Person.eSex.Male, "John Smith", new string[]{ "+1 800 123 4567", "+1 800 123 4568" } ),
+				new Person( Person.eSex.Female, "Mary Jane", new string[]{ "555-1212" } ),
+				new Person( Person.eSex.Other, "Microsoft", new string[]{ "+1 800 642 7676", "1-800-892-5234" } ),
+			};
 
 			Cursor<Person> curPerson;
 			sess.getTable( out curPerson );
@@ -45,17 +45,13 @@ namespace ConsoleDemo
 				Console.WriteLine( "Total count: {0}", rs.Count() );
 
 				Console.WriteLine( "Sorted by sex:" );
-				rs.filterSort( "sex", false );
-				PrintPersons( rs.all() );
+				PrintPersons( rs.orderBy( p => p.sex ) );
 
 				Console.WriteLine( "Males:" );
-				rs.filterFindEqual( "sex", Person.eSex.Male );
-				PrintPersons( rs.all() );
+				PrintPersons( rs.where( p => Person.eSex.Male == p.sex ) );
 
 				Console.WriteLine( @"Names containing ""Smith"":" );
-				rs.filterFindSubstring( "name", "Smith" );
-				// We're using "uniq()" here instead of just "all()", because the tuple index may contain duplicate entries. 
-				PrintPersons( rs.uniq() );
+				PrintPersons( rs.where( p => p.name.Contains( "Smith" ) ) );
 			}
 		}
 
